6851fcf63e9e007fe33504560a2ca76c61ec4512
Naiad Baksh <naiadbaksh@gmail.com>
2022-03-11 15:33:28 -0500

    fixing home page responsiveness, flex-direction: column and media query adding

--- src/components/collection-preview/collection-preview.styles.jsx
+++ src/components/collection-preview/collection-preview.styles.jsx
@@ -1,18 +1,22 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const CollectionPreviewContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    margin-bottom: 30px;
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 30px;
 `;
 
 export const TitleContainer = styled.h1`
-    font-size: 28px;
-    margin-bottom: 25px;
+  font-size: 28px;
+  margin-bottom: 25px;
 `;
 
 export const PreviewContainer = styled.div`
-    display: flex;
-    justify-content: space-between;
+  display: flex;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
 `;
-
